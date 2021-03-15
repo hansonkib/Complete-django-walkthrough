f39215c19037d06e00ffd88854a6e80214e5047e
Hanson kibet <45159396+hansonkib@users.noreply.github.com>
2021-03-15 10:56:08 -0700

    Initial commit

---
+++ accounts/tests/test_view_signup.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.models import User
+# from django.core.urlresolvers import reverse
+from django.urls.base import reverse
+from django.urls import resolve
+from django.test import TestCase
+
+from ..views import signup
+from ..forms import SignUpForm
